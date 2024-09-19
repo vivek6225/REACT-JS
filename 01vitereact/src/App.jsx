e32f9bc07cd4b9661dd2095119ee2a9b3d2c52e1
Vivek Kumar <vivekkumar06102002@gmail.com>
2024-09-19 19:05:30 +0530

    .

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,12 +3,23 @@
 
 
 
-function App() {
+// function App() {
  
 
-  return (
-    <h1>react-js with VITE </h1>
+//   return (
+//     <h1>react-js with VITE </h1>
   
+//   )
+// }
+
+import Chai from "./chai"
+
+function App(){
+  return (
+    <>
+    <Chai/>
+    <h1>chai is ready</h1>
+    </>
   )
 }
 
