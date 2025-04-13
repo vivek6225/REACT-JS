bbece76c99abb8179c9f68dea8ebe77b3b54f423
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-04-13 18:51:22 +0530

    .

--- vitereact/src/main.jsx
+++ vitereact/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDom from 'react-dom/client'
 import App from './App.jsx'
+function MyApp(){
+  return(
+    <div>
+    <h1>Custom App !</h1>
+    </div>
+  )
+}
+// const ReactElement = { 
+//   type: `a`,
+//   props:{
+//     href:"https:/google.com",
+//     target:`-blank`
+//   },
+//   children:`Click me to visit google`
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// }
+ const anotherUser = `chai or react`
+ const anotherElement = (
+  <a href="https:/google.com" target='_blank'>visit google</a>
+)
+const reactElement = React.createElement(
+  `a`,
+  {href: `https://google.com`, target:`_blank`},`click me to visit`,anotherUser
+ )
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  
+   reactElement
+  
 )
