4ffa2b7349cdf5b80158f3d0e00973b7e0cd0b01
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-04-20 22:44:56 +0530

    tailwindprops

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -2,43 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './component/card'
 
 function App() {
+ 
   const [count, setCount] = useState(0)
+  let myObj = {
+    useranme : "vivek kumar gupta",
+    age :23,
+    branch:"information teacnology",
+  }
+  let newArr = [1,2,3,4]
 
   return (
 <>
-
 <h1 className="text-red-500 p-9 rounded-xl m-9 border-4 ">vivek kumar gupta</h1>
+<Card username="vivek" btnText="click me"/>
+<Card username="vivek"/>
 
-<div className="flex flex-col rounded-xl  p-4"
-        style={{
-          border: '0.88px solid',
-  
-          backdropFilter: 'saturate(180%) blur(14px)',
-          background: ' #ffffff0d',
-        }}
-      >
-        <div>
-          <img
-            src="https://res.cloudinary.com/ddcg0rzlo/image/upload/v1652470298/9StaF0UBJfih_df0248.gif"
-            alt="nft-gif"
-            width="400"
-            height="400"
-            className="rounded-xl"
-          />
-        </div>
-        <div className="flex flex-col  rounded-b-xl py-4 ">
-          <div className="flex justify-between">
-            <h1 className="font-RubikBold ">Bored ape nft accidental</h1>
-            <h1 className="font-bold font-RubikBold">Price</h1>
-          </div>
-          <div className="flex  justify-between font-mono">
-            <p>#345</p>
-            <p>0.01</p>
-          </div>
-        </div>
-      </div>
+
+
+
+
 
 </>
      
