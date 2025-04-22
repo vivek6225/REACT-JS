ffbaa6a0351a3400efaeff1dd28ba2167eb06b20
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-04-22 21:24:10 +0530

    couynter.jsx

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -6,15 +6,34 @@ import './App.css'
 function App() {
  
 
-const [counter,setCounter] = useState(15)
+const [counter,setCounter] = useState(0)
   const addvalue = () =>{
-   
-    setCounter(counter+1)
+    // counter = counter + 1
+  //  setCounter(counter+1)
+    setCounter(prevCounter => prevCounter + 1) //It’s called the functional update form, and it works even when React batches state updates.
+    setCounter(prevCounter => prevCounter  + 1)
+  
     // console.log("clicked", counter)
   }
-  const removeValue = () =>{
-    setCounter(counter-1)
-  }
+  // const removeValue = () =>{
+  //  setCounter(counter -1 )
+  //    if(removeValue <= 0){
+  //       return 0;
+  //  }
+
+  //   setCounter(removeValue  - 1 )
+  //   // setCounter(prevCounter => prevCounter -1 )
+
+    
+  // }
+
+  const removeValue = () => {
+    setCounter(prev => {
+      const newValue = prev - 2;
+      return newValue < 0 ? 0 : newValue;
+    });
+  };
+  
   
   return (
     <>
