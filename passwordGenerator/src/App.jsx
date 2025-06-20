370e885844cbe8d665916c89ce1e04d85dbab5aa
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-06-20 19:53:06 +0530

    passwordGenerator

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -9,13 +9,13 @@ function App() {
   const [password,setPassword]= useState(false)
   const [passwordGenerator] = useCallback(() =>{
     let pass=""
-    let char="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    if(numberAllowed) str =+ "0123456789"
-    if(charAllowed) str += "&$@1#^-_=[]{}~`"
+    let str ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "&$@!#^-_=[]{}~`"
 
      for(let i = 1; i <= Array.length; i++){
-    let char = Math.flor(Math.random() * str.length + 1)
-    pass = str.charAt(char) 
+    let char = Math.floor(Math.random() * str.length + 1)
+    pass = str.charAt(char) //  accesses the character at that index.
   }
   setPassword(pass)
   },[length,numberAllowed,charAllopwed,setPassword])
@@ -23,7 +23,7 @@ function App() {
    
   return (
     <>
-    <h1 className="text-4xl text-center text-w ">passwordGenerator</h1>
+   <div classname="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray"></div>
     </>
   )
 }
